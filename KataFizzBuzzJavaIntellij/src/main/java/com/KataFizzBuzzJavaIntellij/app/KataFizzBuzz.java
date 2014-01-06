ba7e915b5c27fd6ed45221e5855ebdb63a5b536a
Jacky <flashlib@gmail.com>
2014-01-06 15:36:00 +0800

    add test unit fifteenShouldSayFizzBuzz.

--- KataFizzBuzzJavaIntellij/src/main/java/com/KataFizzBuzzJavaIntellij/app/KataFizzBuzz.java
+++ KataFizzBuzzJavaIntellij/src/main/java/com/KataFizzBuzzJavaIntellij/app/KataFizzBuzz.java
@@ -1,5 +1,7 @@
 package com.KataFizzBuzzJavaIntellij.app;
 
+import sun.management.counter.StringCounter;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Jacky
@@ -12,18 +14,41 @@ public class KataFizzBuzz {
     public static final String WORD_FIZZ_BUZZ = "FizzBuzz";
 
     public static String sayNumber(int number) {
-        if(number%15 == 0){
-            return  WORD_FIZZ_BUZZ;
+        //if(number%15 == 0){
+       //     return  WORD_FIZZ_BUZZ;
+        //}
+        boolean b = false;
+        String s = "";
+
+        if(number%3 == 0 || includeNumber(number,3)){
+            s=WORD_FIZZ;
+            b = true;
         }
-        if(number%3 == 0){
-            return  WORD_FIZZ;
+        if(number%5 == 0 ){
+            s+=WORD_BUZZ;
+            b = true;
         }
-        if(number%5 == 0){
-            return  WORD_BUZZ;
+       // if (number==13){
+        //    return WORD_FIZZ;
+       // }
+        if(!b){
+            s = String.valueOf(number);
         }
-        if (number==13){
-            return WORD_FIZZ;
-        }
-        return "" + number;
+        return s;
+    }
+
+    public static boolean includeNumber(int original, int num){
+
+        String s = String.valueOf(original);
+        return s.contains(String.valueOf(num));
+
+        //for(int i=0; i < s.length(); i++){
+        //    if(s.indexOf(i) == num){
+        //        return true;
+        //    }
+       // }
+
+       // return false;
+
     }
 }
